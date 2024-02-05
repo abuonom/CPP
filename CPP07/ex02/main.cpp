aeaffb16c79848d61c580880ef16d63b0df82e65
Alessio Buonomo <alessio@Alessio.PC>
2024-02-05 19:01:41 +0100

    Added main to CPP07

--- CPP07/ex02/main.cpp
+++ CPP07/ex02/main.cpp
@@ -1,5 +1,7 @@
 #include "Array.hpp"
 #include <iostream>
+#include <stdlib.h>
+#define MAX_VAL 750
 
 int main()
 {
@@ -50,6 +52,63 @@ int main()
 	std::cout << "Array vuoto:" << std::endl;
 	Array<int> emptyArray;
 	std::cout << "Dimensione dell'array vuoto: " << emptyArray.size() << std::endl;
+
+	std::cout << "Main della evaluation" << std::endl;
+
+	Array<int> numbers(MAX_VAL);
+	int *mirror = new int[MAX_VAL];
+	srand(time(NULL));
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		const int value = rand();
+		numbers[i] = value;
+		mirror[i] = value;
+	}
+
+	// SCOPE
+	{
+		Array<int> tmp = numbers;
+		Array<int> test(tmp);
+		std::cout << "Copia dell'array eseguita correttamente" << std::endl;
+	}
+
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		if (mirror[i] != numbers[i])
+		{
+			std::cerr << "didn't save the same value!!" << std::endl;
+			return 1;
+		}
+	}
+
+	try
+	{
+		numbers[-2] = 0;
+	}
+	catch (const std::exception &e)
+	{
+		std::cout << "Operazione con indice negativo gestita correttamente: " << std::endl;
+		std::cerr << e.what() << '\n';
+	}
+
+	try
+	{
+		numbers[MAX_VAL] = 0;
+	}
+	catch (const std::exception &e)
+	{
+		std::cout << "Operazione con indice fuori limite gestita correttamente: " << std::endl;
+		std::cerr << e.what() << '\n';
+	}
+
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		numbers[i] = rand();
+	}
+
+	delete[] mirror;
+
+	std::cout << "Tutto è andato a buon fine!" << std::endl;
 
 	return 0;
 }
